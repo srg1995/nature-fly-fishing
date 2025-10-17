91be13380ab3319fefea71498a23b4de49f63425
Sergio Sacristan <sergiosacristan8@gmail.com>
2025-10-17 08:18:23 +0200

    form hook + zod

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,28 +1,43 @@
 "use client";
-import React, { JSX, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React, { JSX } from "react";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+const schema = z.object({
+  name: z.string().min(4, "El nombre debe tener al menos 4 caracteres"),
+  subject: z.string().min(6, "El Asunto debe tener al menos 6 caracteres"),
+  message: z
+    .string()
+    .min(20, "El mensaje debe tener mínimo 20 caracteres")
+    .max(500, "El mensaje debe tener máximo 500 caracteres"),
+});
+
+type FormData = z.infer<typeof schema>;
 
 export default function Contact(): JSX.Element {
-  const [form, setForm] = useState({
-    name: "",
-    subject: "",
-    message: "",
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
   });
-  const handleSubmit = () => {
+  const [messageCharacters, setMessageCharacters] = React.useState(0);
+
+  const onSubmit = async (data: FormData) => {
+    await new Promise((res) => setTimeout(res, 300));
+
     const body = encodeURIComponent(
-      `Hola mi nombre es ${form.name} y tengo la siguiente consulta:\n\n${form.message}`,
+      `Hola mi nombre es ${data.name} y tengo la siguiente consulta:\n\n${data.message}`,
     );
 
-    window.location.href = `mailto:tuemail@dominio.com?subject=${form.subject}&body=${body}`;
+    window.location.href = `mailto:contacto@natureflyfishing.com?subject=${data.subject}&body=${body}`;
+    setMessageCharacters(0);
+    reset();
   };
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
-    });
-  };
   return (
     <div className="group/design-root relative flex h-auto min-h-screen w-full flex-col overflow-x-hidden">
       <div className="layout-container flex h-full grow flex-col">
@@ -33,53 +48,86 @@ export default function Contact(): JSX.Element {
                 Contáctanos
               </p>
             </div>
-            <form action={handleSubmit}>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
-                <label className="flex min-w-40 flex-1 flex-col">
-                  <p className="text-primary pb-2 text-base leading-normal font-semibold">
+                <div className="flex min-w-40 flex-1 flex-col">
+                  <label className="text-primary pb-2 text-base leading-normal font-semibold">
                     Nombre
-                  </p>
+                  </label>
                   <input
                     placeholder="Tu nombre"
-                    name="name"
-                    onChange={handleChange}
-                    value={form.name}
+                    id="name"
+                    {...register("name")}
                     className="form-input text-tertiary placeholder:text-tertiary border-accent focus:border-accent flex h-14 w-full min-w-0 flex-1 resize-none overflow-hidden rounded-lg border p-[15px] text-base leading-normal font-normal focus:ring-0 focus:outline-0"
                   />
-                </label>
+                  {errors.name && (
+                    <span
+                      className="text-xs text-red-500"
+                      aria-invalid={!!errors.name}
+                    >
+                      {errors.name.message}
+                    </span>
+                  )}
+                </div>
               </div>
               <div className="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
-                <label className="flex min-w-40 flex-1 flex-col">
-                  <p className="textprimaryy pb-2 text-base leading-normal font-semibold">
+                <div className="flex min-w-40 flex-1 flex-col">
+                  <label className="text-primary pb-2 text-base leading-normal font-semibold">
                     Asunto
-                  </p>
+                  </label>
                   <input
-                    name="subject"
-                    placeholder="Asunto"
-                    onChange={handleChange}
+                    id="subject"
+                    placeholder="Asunto de tu consulta"
+                    {...register("subject")}
                     className="form-input text-tertiary placeholder:text-tertiary border-accent focus:border-accent flex h-14 w-full min-w-0 flex-1 resize-none overflow-hidden rounded-lg border p-[15px] text-base leading-normal font-normal focus:ring-0 focus:outline-0"
                   />
-                </label>
+                  {errors.subject && (
+                    <span
+                      className="text-xs text-red-500"
+                      aria-invalid={!!errors.subject}
+                    >
+                      {errors.subject.message}
+                    </span>
+                  )}
+                </div>
               </div>
               <div className="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
-                <label className="flex min-w-40 flex-1 flex-col">
-                  <p className="text-primary pb-2 text-base leading-normal font-semibold">
+                <div className="flex min-w-40 flex-1 flex-col">
+                  <label className="text-primary pb-2 text-base leading-normal font-semibold">
                     Mensaje
-                  </p>
+                  </label>
                   <textarea
                     placeholder="Tu mensaje"
-                    name="message"
-                    onChange={handleChange}
+                    id="message"
+                    {...register("message", {
+                      onChange: (e) =>
+                        setMessageCharacters(e.target.value.length),
+                    })}
                     className="form-input text-tertiary placeholder:text-tertiary border-accent focus:border-accent flex min-h-36 w-full min-w-0 flex-1 resize-none overflow-hidden rounded-lg border p-[15px] text-base leading-normal font-normal focus:ring-0 focus:outline-0"
                   ></textarea>
-                </label>
+                  <label
+                    htmlFor="message"
+                    className="text-xs"
+                    aria-invalid={!!errors.message}
+                  >
+                    {messageCharacters}/500
+                  </label>
+                  {errors.message && (
+                    <span className="text-xs text-red-500">
+                      {errors.message.message}
+                    </span>
+                  )}
+                </div>
               </div>
               <div className="flex justify-start px-4 py-3">
                 <button
+                  disabled={isSubmitting}
                   className="text-primary bg-accent flex h-10 max-w-[480px] min-w-[84px] cursor-pointer items-center justify-center overflow-hidden rounded-lg px-4 text-sm leading-normal font-bold tracking-[0.015em]"
                   type="submit"
                 >
-                  <span className="truncate">Enviar</span>
+                  <span className="truncate">
+                    {isSubmitting ? "Enviando..." : "Enviar"}
+                  </span>
                 </button>
               </div>
             </form>
